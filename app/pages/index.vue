ddca46f50070e3c9146cda36430b73aa3530a2b2
ONEmanLAW <hugoculot.dev@gmail.com>
2026-03-23 15:51:26 +0100

    Added gsap elements. (Just to try).

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { asImageSrc } from "@prismicio/client";
 import { components } from "~/slices";
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const { client } = usePrismic();
 const { data: page } = await useAsyncData("[home]", () =>
@@ -13,6 +15,23 @@ useSeoMeta({
   description: page.value?.data.meta_description,
   ogDescription: page.value?.data.meta_description,
   ogImage: computed(() => asImageSrc(page.value?.data.meta_image)),
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(() => {
+  gsap.to('.square', {
+    x: 200, 
+    duration: 1, 
+    rotation: 360, 
+    ease: 'expo-out', 
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: '.square-list',
+      markers: true,
+      start: 'top center'
+    },
+  });
 });
 
 
@@ -28,6 +47,27 @@ console.log(page.value?.data.highlititedproject);
       <h1> {{ page?.data.highlititedproject.data.title }}</h1>
       <PrismicRichText :field="page?.data.highlititedproject.data.description" />
     </section>
+
+    <div class="square-list">
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+    </div>
+    
     <SliceZone :slices="page?.data.slices ?? []" :components="components" />
   </main>
 </template>
+
+<style>
+  .square {
+    width: 50px;
+    height: 50px;
+    background-color: red;
+  }
+
+  .square-list {
+    margin: 50vh 0;
+  }
+</style>
